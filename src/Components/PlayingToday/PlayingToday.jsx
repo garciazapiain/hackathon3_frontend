13bcf55cc2cffa731d3bcb81a571997460336d7d
Sam Judge <samuel.judge.92@gmail.com>
2019-10-18 14:10:46 +0200

    come together

--- src/Components/PlayingToday/PlayingToday.jsx
+++ src/Components/PlayingToday/PlayingToday.jsx
@@ -4,6 +4,12 @@ export default function PlayingToday(props) {
     return (
         <>
             PLAYING TODAY
+
+            {props.data.map( movie => (
+                <li>
+                    {movie.name} : {movie.begins_at}
+                </li>
+            ))}
         </>
     )
 }
